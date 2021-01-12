344efe3fcec2239a093cb0f94ef8e6bb091112fb
Danielstep4 <63352307+Danielstep4@users.noreply.github.com>
2021-01-12 16:44:16 +0200

    Edited the PUT

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@
 const { ResumeToken } = require('mongodb');
 const mongoose = require('mongoose');
 module.exports = function (app) {
-  mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+  mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
   const db = mongoose.connection;
   db.on('error', console.error.bind(console, 'connection error'));
   const schema = mongoose.Schema({
@@ -69,20 +69,37 @@ module.exports = function (app) {
     .put(function (req, res){
       let project = req.params.project;
       let id = req.body["_id"]
+      if(!id) {
+        res.json({
+          error: 'missing _id'
+        })
+        return console.log('user tried to update but no id found.')
+      }
+      delete req.body["_id"]
+      if(!Object.keys(req.body).length) {
+        res.json({
+          error: 'no update field(s) sent', 
+          '_id': id
+        })
+        return console.log('no update field(s) sent')
+      }
       let toUpdate = { 
         ...req.body, 
         updated_on: Date.now()
        }
-      delete toUpdate["_id"]
       Issue.findByIdAndUpdate(id, toUpdate, {new: true}, (err, issue) => {
         if(err) {
           res.json({
-            error: 'no update field(s) sent', '_id': id
+            error: 'could not update', 
+            '_id': id
           })
           return console.error(err)
         }
         console.log(`User with ${id} has been updated`)
-        res.json(issue)
+        res.json({
+           result: 'successfully updated',
+            '_id': id
+           })
       })
     })
     
