5344947a1dc29ced8a658c02f8cafaa722fff158
Danielstep4 <63352307+Danielstep4@users.noreply.github.com>
2021-01-12 11:40:05 +0200

    connected to mongodb atlas, created shcema and model.

--- routes/api.js
+++ routes/api.js
@@ -1,17 +1,29 @@
 'use strict';
-
+const mongoose = require('mongoose');
 module.exports = function (app) {
-
+  mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+  const db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error'));
+  const schema = mongoose.Schema({
+    issue_title: { type: String, required: true },
+    issue_text: { type: String, required: true },
+    created_on: { type: Date, default: Date.now },
+    updated_on: { type: Date, default: Date.now },
+    created_by: { type: String, required: true },
+    assigned_to: String,
+    open: Boolean,
+    status_text: String
+  })
+  const Issue = mongoose.model('Issue', schema)
   app.route('/api/issues/:project')
   
     .get(function (req, res){
       let project = req.params.project;
-      
+
     })
     
     .post(function (req, res){
       let project = req.params.project;
-      
     })
     
     .put(function (req, res){
